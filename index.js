b32ca4886c9273610c62d493c163a9a66630f8bf
Harkerbest <103395523+Harkerbest@users.noreply.github.com>
2023-03-11 17:25:16 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const countdown = document.querySelector("#time");
+const endDate = new Date("2022-06-07 00:00:00");
+
+setInterval(() => {
+  const now = new Date();
+  const distance = endDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  countdown.innerHTML = `${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒`;
+}, 1000);
